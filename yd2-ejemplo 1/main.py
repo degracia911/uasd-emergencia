87915815fb36f123f52504e6612dec78a7946112
Katia Romero <ruddydegracia@gmail.com>
2020-09-11 15:14:20 -0400

    guardar cambios

--- yd2-ejemplo 1/main.py
+++ yd2-ejemplo 1/main.py
@@ -1,2 +1,22 @@
 # esto es un com
 print("Funciona")
+# almacenamiento en la memoria
+
+# hacer salida de datos
+print (2 + 4 )
+# entrada de datos
+
+edad = input ("escriba su edad")
+print (input  (edad) + 1 )
+print ("la edad es  " +  edad)
+
+
+# almacenamiento en memoria de los datos 
+
+# expresiones 
+precio = 99.99
+cantidad = 5 
+total = precio * cantidad 
+print (total)
+
+
