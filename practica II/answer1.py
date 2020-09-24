8729f1b03ecb20c946ab840ef9505e892b09a4ee
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-24 12:15:05 -0400

    guardar cambios

---
+++ practica II/answer1.py
@@ -0,0 +1,21 @@
+#1-escriba en pantalla el tipo de dato que retorna la expresion 4 < 2
+c = 4 < 2
+print (c) 
+
+
+
+'''Convertir grados celsius a fahrenheit'''
+c = 20
+f = c * 9/5 + 32
+print(f)
+
+
+'''5- Convertir dolares a pesos'''
+
+d = 100.00
+r = d * 58.45
+print (r)
+
+
+
+
