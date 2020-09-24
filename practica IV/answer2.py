9090d70b1b0dacd216342a7b39ba0b4315c7f754
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-24 11:52:13 -0400

    guardar cambios

---
+++ practica IV/answer2.py
@@ -0,0 +1,9 @@
+class Estudiantes:
+    def __init__(self, promedio):
+        self.promedio = promedio
+        if promedio > 100:
+            print("promedio maximo")
+        else:
+            print("promedio minimo")
+
+Estudiante = Estudiantes(100)
---
+++ practica IV/pregunta1.py
@@ -0,0 +1,25 @@
+class camisa:
+    def __init__(self, tipo, numero, ocasion):
+        self.tipo = tipo
+        self.numero = numero
+        self.ocasion = ocasion
+
+tacones = camisa("camisa", "8", "vestir")
+
+print("camisa", "vestir")
+
+clase  casa :
+    def  __init__ ( self , tama ñ o , ba ñ os , habitaciones ):
+        yo . tama ñ o  =  "37 metros cuadrados"
+        yo . ba ñ os  =  1
+        yo . habitaciones  =  1
+
+    def  display ( auto ):
+        print ( f "Tamaño:    { self . tama ñ o }   Baños: { self . ba ñ os }   habitaciones { self . habitaciones } " )
+
+ carro de clase :
+    def  __init__ ( self , marca , color , asientos ):
+        yo . marca  =  "toyota"
+        yo . color  =  "azul"
+        yo . asientos  =  4
+
