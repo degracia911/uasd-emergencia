a65e02d083b0260bfa12fc0c4d8b255e1baa69b8
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-24 09:39:44 -0400

    guardar cambios

---
+++ practica IV/answe1.py
@@ -0,0 +1,21 @@
+class Curso:
+     def__init__(self, titulo, cupos, participantes):
+     self.titulo = titulo
+     self.cupos = cupos
+     self.participantes = participantes
+     def __init__(self, titulo, cupos, participantes):
+         self.titulo = titulo
+         self.cupos = cupos
+         self.participantes = participantes
+
+ Primeros auxilios = Curso("Python", "12", "8")
+
+ print("titulo:", self.titulo)
+ print("participante;" self.participantes)
+ print("Primeros auxiios.cupos")
+
+
+ 
+
+
+
