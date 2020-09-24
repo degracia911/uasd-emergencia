1fc1b74bb5a47f42ee5c5c220ba34324bb57d7c5
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-24 11:56:17 -0400

    guardar cambios

---
+++ practica IV/answer3.py
@@ -0,0 +1,34 @@
+class Suma:
+    def __init__(self, numero1, numero2):
+       self.numero1 = numero1
+       self.numero2 = numero2
+       resultado = numero1 + numero2
+       print(resultado)
+
+class Resta:
+    def __init__(self, numer1, numer2):
+        self.numer1 = numer1
+        self.numer2 = numer2
+        resultado = numer1 - numer2
+        print(resultado)
+
+class Multiplicacion:
+    def __init__(self, nume1, nume2):
+        self.nume1 = nume1
+        self.nume2 = nume2
+        resultado = nume1 * nume2
+        print(resultado)
+
+class Division:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num1 = num2
+        resultado = num1 / num2
+        print(resultado)
+
+suma = sum(1,2)
+rest = resta(2,3)
+Mult = multiplicacion(4,5)
+div = division(6,7)
+
+
