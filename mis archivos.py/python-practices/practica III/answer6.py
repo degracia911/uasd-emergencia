c83674af4ae36001e5f62f21849b070b9dcb467c
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-23 23:04:42 -0400

    guardar cambios

---
+++ mis archivos.py/python-practices/practica III/answer6.py
@@ -0,0 +1,12 @@
+x = int (input ("digite su numero de cedula: "))
+def cedula ():
+    if cedula ==76:
+    print (" cedula es valida")
+
+
+else: 
+    print ("cedula es invalida ")
+    cedula()
+
+
+
