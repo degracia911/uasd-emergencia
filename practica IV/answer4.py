f5bead75f0868b42b7ac99e3e536be272f492361
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-24 11:59:36 -0400

    guardar cambios

---
+++ practica IV/answer4.py
@@ -0,0 +1,14 @@
+class Personaje:
+   def __init__(self, MoverArriba, MoverAbajo, MoverDerecha, MoverIzquierda):
+       self.MoverArriba = MoverArriba
+       self.MoverAbajo = MoverAbajo
+       self.MoverDerecha = MoverDerecha
+       self.MoverIzquierda = MoverIzquierda
+
+class Mario(personaje):
+    pass
+
+class Koopa(personaje):
+    pass
+
+
