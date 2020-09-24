9aa2a785390be72afc023470bcbdeda7fffbc9bc
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-23 20:51:37 -0400

    guardar cambios

---
+++ practica1/answer7.py
@@ -0,0 +1,32 @@
+# Almacene cuatro notas 90,95,77, 92 y las promedie. Al final debe decir su calificación en letras A, B,C,D, E ó F. 
+
+promedio = (90+95+77+92)/4 
+if(promedio >= 90 ):
+   print("A")
+elif(promedio < 90 and promedio > 85):
+    print("B")   
+elif(promedio < 80 and promedio > 75):
+    print("C")
+elif(promedio < 70 and promedio > 65):
+        print("D")
+elif(promedio < 60 and promedio > 55):
+        print("E")
+elif(promedio < 50 and promedio > 45):
+       print("F")
+       
+
+       
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
